f4ebcb8a0a0a3cbefa7b63febe2ff4c162bc5532
German Makarov <german@appello.com.au>
2023-01-24 16:15:31 +0000

    Merged in add-tabs-animation (pull request #39)
    add tabs animation
    
    * add tabs animation

--- components/common/Tabs/index.tsx
+++ components/common/Tabs/index.tsx
@@ -1,7 +1,7 @@
 import './styles.scss';
 
 import clsx from 'clsx';
-import React, { ReactElement, ReactNode, useState } from 'react';
+import React, { ReactElement, ReactNode, useLayoutEffect, useRef, useState } from 'react';
 
 export interface Tab {
   title: ReactNode;
@@ -11,32 +11,51 @@ export interface Tab {
 
 interface Props<TTab> {
   items: TTab[];
+  contentClassName?: string;
 }
 
-export const Tabs = <TTab extends Tab>({ items }: Props<TTab>): ReactElement => {
+export const Tabs = <TTab extends Tab>({ items, contentClassName }: Props<TTab>): ReactElement => {
   const [selectedTabIndex, setSelectedTabIndex] = useState<number>(0);
   const element = items[selectedTabIndex].element;
 
+  const headListRef = useRef<HTMLUListElement>(null);
+  const activeLineRef = useRef<HTMLDivElement>(null);
+
+  useLayoutEffect(() => {
+    if (!headListRef.current || !activeLineRef.current) {
+      return;
+    }
+    const headListItems = Array.from(headListRef.current.children) as HTMLLIElement[];
+    const activeTabElement = headListItems[selectedTabIndex];
+    if (activeTabElement) {
+      const { offsetLeft, offsetWidth } = activeTabElement;
+      activeLineRef.current.style.width = `${offsetWidth}px`;
+      activeLineRef.current.style.transform = `translate3d(${offsetLeft}px, 0, 0)`;
+    }
+  }, [selectedTabIndex]);
+
   return (
     <div className="tabs">
-      <ul className="tabs__head">
-        <li className="tabs__head-item">
+      <div className="tabs__head">
+        <ul ref={headListRef} className="tabs__head-list">
           {items.map((item, index) => (
-            <button
-              key={index}
-              type="button"
-              onClick={() => setSelectedTabIndex(index)}
-              disabled={item.disabled}
-              className={clsx('tabs__head-button', {
-                'tabs__head-button--active': index === selectedTabIndex,
-              })}
-            >
-              {item.title}
-            </button>
+            <li key={index} className="tabs__head-item">
+              <button
+                type="button"
+                onClick={() => setSelectedTabIndex(index)}
+                disabled={item.disabled}
+                className={clsx('tabs__head-button', {
+                  'tabs__head-button--active': index === selectedTabIndex,
+                })}
+              >
+                {item.title}
+              </button>
+            </li>
           ))}
-        </li>
-      </ul>
-      <div className="tabs__body">{element}</div>
+        </ul>
+        <div ref={activeLineRef} className="tabs__head-line" />
+      </div>
+      <div className={clsx('tabs__body', contentClassName)}>{element}</div>
     </div>
   );
 };
