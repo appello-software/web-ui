46964091295f3cbc97bfecb41d5ed64e134506ee
gmakarov <german@appello.com.au>
2023-05-26 15:18:16 +0200

    feat(Badge): add orange color and new `filled` variation

--- src/components/common/Badge/index.tsx
+++ src/components/common/Badge/index.tsx
@@ -3,21 +3,27 @@ import './styles.scss';
 import clsx from 'clsx';
 import React, { FC, ReactNode } from 'react';
 
+import { Icon } from '~/components';
+
 export enum BadgeColor {
   GREEN = 'green',
   GRAY = 'gray',
   BLUE = 'blue',
   RED = 'red',
+  ORANGE = 'orange',
 }
 
 export interface BadgeProps {
   children: ReactNode;
   color: BadgeColor;
+  icon?: string;
+  filled?: boolean;
 }
 
-export const Badge: FC<BadgeProps> = ({ children, color }) => {
+export const Badge: FC<BadgeProps> = ({ children, color, icon, filled }) => {
   return (
-    <div className={clsx('badge', `badge--${color}`)}>
+    <div className={clsx('badge', `badge--${color}`, { 'badge--filled': filled })}>
+      {icon !== undefined && <Icon name={icon} size={14} className="badge__icon" />}
       <p className="badge__text">{children}</p>
     </div>
   );
