b392ebc9f2a2eb7fc85568083b6be901e89d0b99
Victor Razdorov <victor.razdorov@gmail.com>
2024-07-31 23:17:42 +0300

    feat(TICS008): improve sidebar logic

--- src/components/common/Sidebar/components/NavItem/index.tsx
+++ src/components/common/Sidebar/components/NavItem/index.tsx
@@ -1,4 +1,11 @@
 import { useSwitchValue } from '@appello/common';
+import {
+  autoUpdate,
+  safePolygon,
+  useFloating,
+  useHover,
+  useInteractions,
+} from '@floating-ui/react';
 import clsx from 'clsx';
 import React, { useLayoutEffect } from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
@@ -14,15 +21,48 @@ interface Props {
 
 export const NavItem: React.FC<Props> = ({ item, className }) => {
   const location = useLocation();
+
+  const { value: isHover, set: setHover } = useSwitchValue(false);
+
   const {
     value: isSubItemsOpen,
     toggle: toggleSubItems,
     set: setSubItemsOpen,
   } = useSwitchValue(false);
 
+  const { refs, floatingStyles, context } = useFloating({
+    open: isHover,
+    onOpenChange: setHover,
+    whileElementsMounted: autoUpdate,
+    placement: 'right-start',
+    strategy: 'fixed',
+  });
+
+  const hover = useHover(context, {
+    handleClose: safePolygon({
+      blockPointerEvents: true,
+    }),
+  });
+
+  const { getReferenceProps, getFloatingProps } = useInteractions([hover]);
+
   useLayoutEffect(() => {
     setSubItemsOpen(location.pathname.startsWith(item.link));
   }, [item.link, location.pathname, setSubItemsOpen]);
+
+  const renderNestedItems = () =>
+    item?.items?.map((subItem, index) => (
+      <li key={index}>
+        <NavLink
+          end
+          className={({ isActive }) => clsx('sidebar__item', { 'sidebar__item--active': isActive })}
+          to={subItem.link}
+        >
+          <span className="sidebar__item-title">{subItem.title}</span>
+          {subItem?.navRightContent?.(subItem)}
+        </NavLink>
+      </li>
+    ));
 
   return (
     <li className={className}>
@@ -30,6 +70,8 @@ export const NavItem: React.FC<Props> = ({ item, className }) => {
         <>
           <button
             className={clsx('sidebar__item', { 'sidebar__item--expanded': isSubItemsOpen })}
+            ref={refs.setReference}
+            {...getReferenceProps()}
             type="button"
             onClick={toggleSubItems}
           >
@@ -38,24 +80,24 @@ export const NavItem: React.FC<Props> = ({ item, className }) => {
             {item?.navRightContent?.(item)}
             <Icon className="sidebar__chevron" name="down-arrow" />
           </button>
-          <ul className="sidebar__submenu">
-            {item.items.map((subItem, index) => (
-              <li key={index}>
-                <NavLink
-                  end
-                  className={({ isActive }) =>
-                    clsx('sidebar__item', { 'sidebar__item--active': isActive })
-                  }
-                  to={subItem.link}
-                >
-                  <span className="sidebar__item-title">{subItem.title}</span>
-                  {subItem?.navRightContent?.(subItem)}
-                </NavLink>
-              </li>
-            ))}
-          </ul>
+
+          <ul className="sidebar__submenu">{renderNestedItems()}</ul>
+
+          {/* Show only on <1400px */}
+          {/* @TODO unify it */}
+          {isHover && (
+            <div
+              className="sidebar__floating"
+              ref={refs.setFloating}
+              style={floatingStyles}
+              {...getFloatingProps()}
+            >
+              <ul className="sidebar__floating-menu">{renderNestedItems()}</ul>
+            </div>
+          )}
         </>
       )}
+
       {!item.items && (
         <NavLink
           className={({ isActive }) => clsx('sidebar__item', { 'sidebar__item--active': isActive })}
