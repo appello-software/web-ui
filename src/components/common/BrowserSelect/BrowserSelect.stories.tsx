e0a2ad0f2ec143e0904854c829a782c8ecf02ea7
Victor Razdorov <victor.razdorov@gmail.com>
2024-10-08 01:44:09 +0300

    update: update storybook

--- src/components/common/BrowserSelect/BrowserSelect.stories.tsx
+++ src/components/common/BrowserSelect/BrowserSelect.stories.tsx
@@ -19,6 +19,7 @@ export const Standard: Story = {
     options: [
       { label: 'Option 1', value: 'option-1' },
       { label: 'Option 2', value: 'option-2' },
+      { label: 'Option 3', value: 'option-3' },
     ],
     value: 'option-2',
     children: 'Label',
