b869281b42cbf4edc47fb07f324b6bb4a25df46f
Victor Razdorov <victor.razdorov@gmail.com>
2024-10-18 13:26:25 +0300

    build: Update main packages husky / rollups / storybooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
-{
-  "extends": ["@appello/eslint-config"],
-  "rules": {
+/** @type {import("eslint").Config} */
+module.exports = {
+  extends: ["@appello/eslint-config"],
+  rules: {
     "import/no-extraneous-dependencies": "off",
     "react/no-unused-prop-types": "off",
     "import/no-default-export": "off"
--- lint-staged.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  "./src/**/*.{ts,tsx,scss}": "npm run prettier",
-  "./src/**/*.{js,ts,jsx,tsx}": "npm run lint"
-}
